0b651ef65a042311e61d9c236a72662df1a0b662
Oleh Mykolaichuk <ooolehmy@gmail.com>
2021-04-14 20:56:06 +0300

    First version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Contacts from "./components/Contacts"
+import "./components/style.css"
+
+function App() {
+  return (
+    <div className = "conteiner">
+      <Contacts/>
+    </div>
+  );
+}
+
+export default App;
